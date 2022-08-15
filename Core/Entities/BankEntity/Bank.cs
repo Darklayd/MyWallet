072865799f9f1e61e69a9bd18b0ba9f3701985e8
Darklayd <mrdarklayd@gmail.com>
2022-08-15 16:53:24 +0300

    create entities

---
+++ Core/Entities/BankEntity/Bank.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.CardEntity;
+using System.Collections.ObjectModel;
+
+namespace Core.Entities.BankEntity
+{
+    public class Bank
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Collection<Card> Cards { get; set; }
+    }
+}
---
+++ Core/Entities/TypesMoney/TypeMoney.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using Core.Entities.CashEntity;
+
+namespace Core.Entities.TypesMoney
+{
+    public class TypeMoney
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Collection<Cash> Cash { get; set; }
+    }
+}
